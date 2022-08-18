fadcef32d6b0f4b8284deb6d83a1a25310795f7b
Yura Menschikov <yura.menschikov@icloud.com>
2022-08-18 20:23:49 +0300

    Add new colors

--- src/constants/Color.ts
+++ src/constants/Color.ts
@@ -1,7 +1,8 @@
 export const Color = {
-    red: 'red',
+    red: '#FF0000',
     white: '#FFFFFF',
     orange_tan_crayola: '#D69965',
     cream_alabaster: '#F4F1E4',
-    green_msu: '#233D3A'
+    green_msu: '#233D3A',
+    black: '#000000'
 }
