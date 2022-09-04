17a2655647961431ba677fc2dd073952b4c914f0
Yura Menschikov <yura.menschikov@icloud.com>
2022-09-05 00:05:45 +0300

    Add reusable components like button and header

---
+++ src/reusableComponents/CustomHeader.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {Text, StyleSheet, TouchableOpacity, Image, View } from "react-native";
+
+export const CustomHeader = (): JSX.Element => {
+    return (
+        <View style={styles.container}>
+            {/*<Image source={require('../assets/backButton/backButton.png')}/>*/}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row'
+    }
+})
