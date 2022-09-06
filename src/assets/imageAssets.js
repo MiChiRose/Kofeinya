1104e588619602a6b084c7cd9567cbb1d3c522df
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2022-09-06 20:19:25 +0300

    Add constants assets

---
+++ src/assets/imageAssets.js
@@ -0,0 +1,11 @@
+import home from "../assets/home/home.png"
+import account from "../assets/account/account.png"
+import cart from "../assets/cart/cart.png"
+import heart from "../assets/heart/heart.png"
+
+export default {
+    home,
+    account,
+    cart,
+    heart
+}
