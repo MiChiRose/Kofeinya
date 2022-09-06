a51ce4fcfe70c3d45d20d9e670063f788d112a48
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2022-09-06 20:22:44 +0300

    Update bottomnavigation

--- src/navigation/bottomNavigation.tsx
+++ src/navigation/bottomNavigation.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Image} from "react-native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import routeNames from "./routeNames";
+import imageAssets from "../assets/imageAssets";
+
 import {MenuScreen} from "../screens/menu/menuScreen";
-import {TabBar} from "./tabbar/tabbar";
-import {Main} from "../screens/Main";
+import {CartScreen} from "../screens/cart/cartScreen";
+import {FavoritesScreen} from "../screens/favorites/favoritesScreen";
+import {AccountScreen} from "../screens/account/accountScreen";
+import {Color} from "../constants/Color";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,13 +18,37 @@ export const BottomNavigation = () => {
         <>
             <Tab.Navigator
                 initialRouteName={routeNames.MenuScreen}
-                tabBar={(props) => <TabBar {...props}/>}
-                screenOptions={{
+                screenOptions={({route}) => ({
+                    tabBarLabel: () => {
+                        return null
+                    },
+                    tabBarIcon: ({focused}) => {
+                        let iconName;
+
+                        if (route.name === routeNames.MenuScreen) {
+                            iconName = imageAssets.home
+                        } else if (route.name === routeNames.CartScreen) {
+                            iconName = imageAssets.cart
+                        } else if (route.name === routeNames.FavoritesScreen) {
+                            iconName = imageAssets.heart
+                        } else if (route.name === routeNames.AccountScreen) {
+                            iconName = imageAssets.account
+                        }
+
+                        return (
+                            <Image
+                                source={iconName}
+                                style={{tintColor: focused ? Color.orange_tan_crayola : Color.black}}
+                            />
+                        );
+                    },
                     headerShown: false
-                }}
+                })}
             >
                 <Tab.Screen name={routeNames.MenuScreen} component={MenuScreen}/>
-                <Tab.Screen name={routeNames.MainScreen} component={Main}/>
+                <Tab.Screen name={routeNames.CartScreen} component={CartScreen}/>
+                <Tab.Screen name={routeNames.FavoritesScreen} component={FavoritesScreen}/>
+                <Tab.Screen name={routeNames.AccountScreen} component={AccountScreen}/>
             </Tab.Navigator>
         </>
     )
