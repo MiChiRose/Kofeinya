8a00a53abda5e30a2ecc784a716a01cf5454aeb4
C5345425 <971656316607f16c7e45d3e64a7cfcabf047cc96>
2022-09-06 20:20:25 +0300

    Add colors

--- src/constants/Color.ts
+++ src/constants/Color.ts
@@ -4,5 +4,6 @@ export const Color = {
     orange_tan_crayola: '#D69965',
     cream_alabaster: '#F4F1E4',
     green_msu: '#233D3A',
-    black: '#000000'
+    black: '#000000',
+    transparent: '#00000000'
 }
